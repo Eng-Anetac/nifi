7036045ded032834d62a0d7e3dda949359b4b0f5
Greg Solovyev <greg@anetac.com>
2023-09-27 09:01:04 -0700

    ANE-478: hack toolkit to produce less output

--- nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/util/JacksonUtils.java
+++ nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/util/JacksonUtils.java
@@ -16,23 +16,41 @@
  */
 package org.apache.nifi.toolkit.cli.impl.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import org.apache.nifi.web.api.dto.ComponentDTO;
+import org.apache.nifi.web.api.dto.ControllerServiceDTO;
+import org.apache.nifi.web.api.dto.ParameterContextDTO;
+import org.apache.nifi.web.api.dto.ProcessorConfigDTO;
+import org.apache.nifi.web.api.dto.PropertyDescriptorDTO;
+import org.apache.nifi.web.api.entity.ControllerServiceReferencingComponentEntity;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Map;
+import java.util.Set;
 
 public class JacksonUtils {
+
+    public abstract class ProcessorConfigMixin {
+        @JsonIgnore private Map<String, PropertyDescriptorDTO> descriptors;
+        @JsonIgnore private Set<ControllerServiceReferencingComponentEntity> referencingComponents;
+    }
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
     static {
         MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         MAPPER.setDefaultPropertyInclusion(JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL));
-        MAPPER.setAnnotationIntrospector(new JaxbAnnotationIntrospector(MAPPER.getTypeFactory()));
+        //MAPPER.setAnnotationIntrospector(new JaxbAnnotationIntrospector(MAPPER.getTypeFactory()));
         MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        MAPPER.addMixIn(Object.class, ProcessorConfigMixin.class);
+        MAPPER.addMixIn(ComponentDTO.class, ProcessorConfigMixin.class);
+        MAPPER.addMixIn(ControllerServiceDTO.class, ProcessorConfigMixin.class);
+        MAPPER.addMixIn(ProcessorConfigDTO.class, ProcessorConfigMixin.class);
+        MAPPER.addMixIn(ParameterContextDTO.class, ProcessorConfigMixin.class);
     }
 
     private static final ObjectWriter OBJECT_WRITER = MAPPER.writerWithDefaultPrettyPrinter();
