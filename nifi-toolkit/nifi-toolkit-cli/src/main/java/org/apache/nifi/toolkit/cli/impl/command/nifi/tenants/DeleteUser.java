40dcb1022915fe94c8fad9f3bc25124e226705e5
greg-anetac <greg@anetac.com>
2024-02-01 13:25:30 -0800

    ANE-936: add delete-user commands to nifi and registry toolkits
    ANE-936: add license block
    
    ANE-936: update result type to VoidResult
    
    ANE-936: add pg-delete command

---
+++ nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/command/nifi/tenants/DeleteUser.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.toolkit.cli.impl.command.nifi.tenants;
+
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.nifi.toolkit.cli.api.Context;
+import org.apache.nifi.toolkit.cli.impl.client.nifi.NiFiClient;
+import org.apache.nifi.toolkit.cli.impl.client.nifi.NiFiClientException;
+import org.apache.nifi.toolkit.cli.impl.client.nifi.TenantsClient;
+import org.apache.nifi.toolkit.cli.impl.command.CommandOption;
+import org.apache.nifi.toolkit.cli.impl.command.nifi.AbstractNiFiCommand;
+import org.apache.nifi.toolkit.cli.impl.result.VoidResult;
+import org.apache.nifi.web.api.entity.UserEntity;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Command to delete a Nifi user.
+ */
+public class DeleteUser extends AbstractNiFiCommand<VoidResult> {
+
+        public DeleteUser() {
+            super("delete-user", VoidResult.class);
+        }
+
+        @Override
+        public String getDescription() {
+            return "Deletes the user with the given id.";
+        }
+
+        @Override
+        protected void doInitialize(final Context context) {
+            addOption(CommandOption.USER_ID.createOption());
+        }
+
+        @Override
+        public VoidResult doExecute(final NiFiClient client, final Properties properties)
+                throws NiFiClientException, IOException, MissingOptionException {
+
+            final String userId = getRequiredArg(properties, CommandOption.USER_ID);
+            UserEntity userEntity = client.getTenantsClient().getUser(userId);
+            if (userEntity == null) {
+                throw new NiFiClientException("User with id " + userId + " does not exist");
+            }
+            final TenantsClient tenantsClient = client.getTenantsClient();
+            tenantsClient.deleteUser(userEntity);
+            return new VoidResult();
+        }
+}
---
+++ nifi-toolkit/nifi-toolkit-cli/src/main/java/org/apache/nifi/toolkit/cli/impl/command/registry/tenant/DeleteUser.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.toolkit.cli.impl.command.registry.tenant;
+
+import org.apache.commons.cli.ParseException;
+import org.apache.nifi.registry.authorization.User;
+import org.apache.nifi.registry.client.NiFiRegistryClient;
+import org.apache.nifi.registry.client.NiFiRegistryException;
+import org.apache.nifi.registry.client.TenantsClient;
+import org.apache.nifi.toolkit.cli.api.Context;
+import org.apache.nifi.toolkit.cli.impl.command.CommandOption;
+import org.apache.nifi.toolkit.cli.impl.command.registry.AbstractNiFiRegistryCommand;
+import org.apache.nifi.toolkit.cli.impl.result.VoidResult;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Command to delete a Nifi Registry user.
+ */
+public class DeleteUser extends AbstractNiFiRegistryCommand<VoidResult> {
+
+        public DeleteUser() {
+            super("delete-user", VoidResult.class);
+        }
+
+        @Override
+        public String getDescription() {
+            return "Deletes the user with the given id.";
+        }
+
+        @Override
+        protected void doInitialize(final Context context) {
+            addOption(CommandOption.USER_ID.createOption());
+        }
+
+        @Override
+        public VoidResult doExecute(final NiFiRegistryClient client, final Properties properties)
+                throws IOException, NiFiRegistryException, ParseException {
+
+            final TenantsClient tenantsClient = client.getTenantsClient();
+
+            final String userId = getRequiredArg(properties, CommandOption.USER_ID);
+            final User user = tenantsClient.getUser(userId);
+            if (user == null) {
+                throw new NiFiRegistryException("User does not exist with id " + userId);
+            }
+
+            tenantsClient.deleteUser(userId);
+
+            return new VoidResult();
+        }
+
+}
