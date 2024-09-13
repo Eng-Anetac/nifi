ed673c96d2f801830e2b7d4301be2e63c8bb9af4
greg-anetac <greg@anetac.com>
2024-09-12 17:42:57 -0700

    ANE-2069: disable manifest test, because it looks for hadoop-bundle

--- nifi-manifest/nifi-runtime-manifest-test/src/test/java/org/apache/nifi/runtime/manifest/TestRuntimeManifest.java
+++ nifi-manifest/nifi-runtime-manifest-test/src/test/java/org/apache/nifi/runtime/manifest/TestRuntimeManifest.java
@@ -35,6 +35,7 @@ import org.apache.nifi.components.resource.ResourceCardinality;
 import org.apache.nifi.components.resource.ResourceType;
 import org.apache.nifi.components.state.Scope;
 import org.apache.nifi.scheduling.SchedulingStrategy;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
@@ -54,6 +55,7 @@ class TestRuntimeManifest {
 
     public static final String LIST_HDFS_DEFAULT_SCHEDULE_TIME = "1 min";
 
+    @Disabled("This test is disabled, because we are skipping hadoop and kafka processors in the runtime manifest")
     @Test
     void testRuntimeManifest() throws IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
@@ -93,6 +95,7 @@ class TestRuntimeManifest {
         final List<Bundle> bundles = runtimeManifest.getBundles();
         assertNotNull(bundles);
         assertTrue(bundles.size() > 0);
+
 
         // Verify ListHDFS definition
         final ProcessorDefinition listHdfsDefinition = getProcessorDefinition(bundles, "nifi-hadoop-nar", "org.apache.nifi.processors.hadoop.ListHDFS");
